6b936f050e9725ee54ebfc7f00a3b1a1a01c5985
hemasri098 <59275471+hemasri098@users.noreply.github.com>
2020-03-26 10:34:36 +0530

    added loggers and exceptional handling

--- Assignment8.java
+++ Assignment8.java
@@ -43,15 +43,75 @@ public class Assignment8 {
         }
     }
     public static void main(String args[]) {
+        Logger LOGGER = Logger.getAnonymousLogger();
+        try{package com.company.JavaAssignment8;
+import java.util.logging.Logger;
+
+class MyException1 extends Exception{
+    int a;
+    MyException1(int b) {
+        a = b;
+    }
+    public String toString(){
+        return ("Exception Number =  " + a) ;
+    }
+}
+class MyException2 extends Exception{
+    int a;
+    MyException2(int b) {
+        a = b;
+    }
+    public String toString(){
+        return ("Exception Number =  " + a) ;
+    }
+}
+class MyException3 extends Exception{
+    int a;
+    MyException3(int b) {
+        a = b;
+    }
+    public String toString(){
+        return ("Exception Number =  " + a) ;
+    }
+}
+
+public class Assignment8 {
+    public static void throwAll(int a) throws MyException1, MyException2, MyException3 {
+        try {
+            if (a == 1)
+                throw new MyException1(1);
+            else if (a == 2)
+                throw new MyException2(2);
+            else if (a == 3)
+                throw new MyException3(3);
+        }
+        catch (Exception e) {
+            throw e;
+        }
+    }
+    public static void main(String args[]) {
+        Logger LOGGER = Logger.getAnonymousLogger();
         try{
+            throwAll(4);
+
+        }
+        catch(MyException1 | MyException2 | MyException3 e ) {
+            LOGGER.info("Exception: " + e);
+        }
+        finally {
+            LOGGER.info("Finally block is getting executed...");
+        }
+    }
+}
+
            throwAll(4);
 
         }
         catch(MyException1 | MyException2 | MyException3 e ) {
-            System.out.println(e);
+            LOGGER.INFO("Exception: " + e);
         }
         finally {
-            System.out.println("Finally block is getting executed...");
+            LOGGER.info("Finally block is getting executed...");
         }
     }
 }
