fd1c00c999f49130ad4393439ceba02dc08671ce
hemasri098 <59275471+hemasri098@users.noreply.github.com>
2020-03-26 11:18:53 +0530

    added comments

--- Assignment8.java
+++ Assignment8.java
@@ -1,52 +1,6 @@
-package com.company;
-
-class MyException1 extends Exception{
-    int a;
-    MyException1(int b) {
-        a = b;
-    }
-    public String toString(){
-        return ("Exception Number =  " + a) ;
-    }
-}
-class MyException2 extends Exception{
-    int a;
-    MyException2(int b) {
-        a = b;
-    }
-    public String toString(){
-        return ("Exception Number =  " + a) ;
-    }
-}
-class MyException3 extends Exception{
-    int a;
-    MyException3(int b) {
-        a = b;
-    }
-    public String toString(){
-        return ("Exception Number =  " + a) ;
-    }
-}
-
-public class Assignment8 {
-    public static void throwAll(int a) throws MyException1, MyException2, MyException3 {
-        try {
-            if (a == 1)
-                throw new MyException1(1);
-            else if (a == 2)
-                throw new MyException2(2);
-            else if (a == 3)
-                throw new MyException3(3);
-        }
-        catch (Exception e) {
-            throw e;
-        }
-    }
-    public static void main(String args[]) {
-        Logger LOGGER = Logger.getAnonymousLogger();
-        try{package com.company.JavaAssignment8;
+package com.company.JavaAssignment8;
 import java.util.logging.Logger;
-
+//creating three user exceptional classes
 class MyException1 extends Exception{
     int a;
     MyException1(int b) {
@@ -75,9 +29,10 @@ class MyException3 extends Exception{
     }
 }
 
-public class Assignment8 {
-    public static void throwAll(int a) throws MyException1, MyException2, MyException3 {
+class UserException {
+    public static void throwAllExceptions(int a) throws MyException1, MyException2, MyException3 {
         try {
+            //if a is 1, it moves to user exception 1. if a is 2, it moves to user exception 2. Similarly, third one
             if (a == 1)
                 throw new MyException1(1);
             else if (a == 2)
@@ -92,23 +47,10 @@ public class Assignment8 {
     public static void main(String args[]) {
         Logger LOGGER = Logger.getAnonymousLogger();
         try{
-            throwAll(4);
-
+            throwAllExceptions(3); // using one throw handling all exceptions
         }
-        catch(MyException1 | MyException2 | MyException3 e ) {
+        catch(MyException1 | MyException2 | MyException3 e ) { // it catches any exception
             LOGGER.info("Exception: " + e);
-        }
-        finally {
-            LOGGER.info("Finally block is getting executed...");
-        }
-    }
-}
-
-           throwAll(4);
-
-        }
-        catch(MyException1 | MyException2 | MyException3 e ) {
-            LOGGER.INFO("Exception: " + e);
         }
         finally {
             LOGGER.info("Finally block is getting executed...");
